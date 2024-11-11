a57e2ac430a18742ab8ba7c174a10f425705e8cb
Shaiful4802 <office.shaiful@gmail.com>
2024-11-11 11:23:21 +0600

    time

--- js/main.js
+++ js/main.js
@@ -1,50 +1,21 @@
-var display = document.getElementById('display');
-var btns = document.querySelectorAll('.btn button'); 
-
-for(var i = 0; i < btns.length; i++){
-    var btn = btns[i];  
-
-    btn.addEventListener('click', function(e){
-        var text = e.target.innerText;
-
-        if(text === '='){
-            var result = eval(display.value);
-            display.value = result;
-        }
-        else if(text === 'AC'){
-            display.value = '';
-        }
-        else{
-            display.value += text;
-        }
-    });   
-}
-
-
-
-// let form = document.getElementById('mainForm');
-//     console.log(form);
-// form.addEventListener('submit', function(event){
-//     event.preventDefault();
-
-//     var fName = event.target.name.value;
-//     var email = event.target.email.value;
-//     var subject = event.target.subject.value;
-//     var password = event.target.password.value;
-//     var info = event.target.textarea.value;
-    
-//     let informaton = {
-//         FullName : fName,
-//         Email : email,
-//         Subject : subject,
-//         Password : password,
-//         Massege : info,
-//     };
-
-//     console.log(informaton);
-
-// });
+var seconds = document.getElementById('seconds');
+var minutes = document.getElementById('minutes');
+var hours = document.getElementById('hours');
 
 
 
 
+
+setInterval(function(){
+    var date = new Date();
+
+    var newSeconds = date.getSeconds();
+    var newMinutes = date.getMinutes();
+    var newHours = date.getHours();
+
+    seconds.innerText = newSeconds;
+    minutes.innerText = newMinutes;
+    hours.innerText = newHours;
+
+
+}, 1000);
